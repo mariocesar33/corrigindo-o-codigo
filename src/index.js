c4ec5fde0080225e941276bbbb4f1bb9217c4aa9
Mário César Silva <mcgato33@hotmail.com>
2021-05-26 15:13:08 -0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -44,7 +44,7 @@ app.post("/repositories", (request, response) => {
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
-  const updatedRepository = request.body;
+  const { title, url, techs } = request.body;
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
@@ -52,7 +52,12 @@ app.put("/repositories/:id", (request, response) => {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  const repository = { ...repositories[repositoryIndex], ...updatedRepository };
+  const repository = { 
+    ...repositories[repositoryIndex], 
+    title, 
+    url, 
+    techs
+  };
 
   repositories[repositoryIndex] = repository;
 
@@ -64,7 +69,7 @@ app.delete("/repositories/:id", (request, response) => {
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
@@ -84,7 +89,7 @@ app.post("/repositories/:id/like", (request, response) => {
 
   const likes = ++repositories[repositoryIndex].likes;
 
-  return response.json('likes');
+  return response.json({ likes });
 });
 
 module.exports = app;
